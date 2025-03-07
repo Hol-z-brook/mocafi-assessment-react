b9a303848a66d22551c4fdace6595ce120f76cfc
Andrew Holbrook <holz55@gmail.com>
2025-03-07 14:37:29 -0700

    feat: add a breadcrumb component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,12 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
 
 import { AppSidebar } from "../components/app-sidebar";
+import { Breadcrumb, BreadcrumbItem, BreadcrumbLink, BreadcrumbList, BreadcrumbPage, BreadcrumbSeparator } from "../components/ui/breadcrumb";
 import { ThemeProvider } from "../components/theme-provider";
 import { SidebarInset, SidebarProvider, SidebarTrigger } from "../components/ui/sidebar";
-
-import "./globals.css";
+import { Separator } from "@radix-ui/react-separator";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -44,7 +45,23 @@ export default function RootLayout({
               title="Andrew Holbrook | Mocafi Assessment"
             />
             <SidebarInset>
-              <SidebarTrigger />
+              <div className="flex items-center gap-2">
+                <SidebarTrigger />
+                <Separator orientation="vertical" />
+                <Breadcrumb>
+                  <BreadcrumbList>
+                    <BreadcrumbItem className="hidden md:block">
+                      <BreadcrumbLink href="/">
+                        Home
+                      </BreadcrumbLink>
+                    </BreadcrumbItem>
+                    <BreadcrumbSeparator className="hidden md:block" />
+                    <BreadcrumbItem>
+                      <BreadcrumbPage>User Management</BreadcrumbPage>
+                    </BreadcrumbItem>
+                  </BreadcrumbList>
+                </Breadcrumb>
+              </div>
               <div className="flex flex-col gap-4">
                 {children}
               </div>
